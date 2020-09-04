b9ac45b7ba991aa17f3963a68600d545e9aa5773
abdulrahmanAlim <51789733+abdulrahmanAlim@users.noreply.github.com>
2020-09-04 13:58:43 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Onetel Studio Design</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+	<link rel="stylesheet" href="css/lightbox.min.css">
+
+	<script src="js/lightbox.js"></script>
+
+</head>
+<body style="background-color: black">
+
+
+	<!-- Navigation bar -->
+	<div class="container">
+	<nav class="navbar navbar-expand-sm">
+		<a class="navbar-brand" href="#"><img src="images/logo.png"></a>
+	  <ul class="navbar-nav">
+	    <li class="nav-item">
+	      <a class="nav-link" href="index.php">HOME</a>
+	    </li>
+	    <li class="nav-item">
+	      <a class="nav-link" href="#">ABOUT US</a>
+	    </li>
+	    <li class="nav-item">
+	      <a class="nav-link" href="#">PORTFOLIO</a>
+	    </li>
+	    <li class="nav-item">
+	      <a class="nav-link" href="#">CONTACT</a>
+	    </li>
+	  </ul>
+	</nav>
+	</div>
+
+	<!-- Welcome Message and Image -->
+	<div class="home">
+		<div class="container">
+		<div class="row">
+			<div class="col-md-5 col-sm-3"></div>
+			<div class="col-md-7 col-sm-9">
+				<h3>welcome to</h3>
+				<h1>Onetel Design Studio</h1>
+			</div>
+		</div>
+	</div>
+	</div>
+
+	<!-- Section -->
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 col-sm-6">
+			<div class="container-square" style="background-color: #c14000 ">
+				<i class="fa fa-laptop fa-4" aria-hidden="true"></i>
+				<h2>Resposive</h2>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet.</p>
+			</div>
+			</div>
+
+			<div class="col-md-4 col-sm-6">
+			<div class="container-square" style="background-color: #009988 ">
+				<i class="fa fa-mobile"></i>
+				<h2>Easy to use</h2>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet.</p>
+			</div>
+			</div>
+
+			<div class="col-md-4 col-sm-6">
+			<div class="container-square" style="background-color: #ffa500 ">
+				<i class="fa fa-life-ring"></i>
+				<h2>Quick Support</h2>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet.</p>
+			</div>
+			</div>
+		</div>
+	
+	<!-- Second Section -->
+	<div class="container-par">
+		<div class="row">
+          <div class="col-md-7 order-md-2">
+            <h2>Startup Business</h2>
+            <p class="text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet. Dolore magna aliquam erat volutpat. Lorem ipsum dolor sit amet.</p>
+			<p class="text">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet. Dolore magna aliquam erat volutpat.</p>
+			<button class="black-button">Learn more</button>
+          </div>
+
+          <div class="col-md-5 order-md-1">
+            <img src="images/about-img.jpg">
+          </div>
+        </div>
+	</div>
+
+	<!-- Third Section -->
+	<div>
+		<center>
+			<h2 style="color: white; margin-top: 100px;">Recent Projects</h2>
+			<p class="text"><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet. Dolore magna aliquam erat volutpat.</p>
+		
+
+		<div class="row1">
+			<div class="item1">
+				<a href="images/img1.jpg" data-lightbox="gallery">
+				  <img src="images/img1.jpg" alt="image-1">
+				</a>
+			</div>
+
+			<div class="item1">
+				<a  href="images/img2.jpg" data-lightbox="gallery">
+				  <img  src="images/img2.jpg" alt="image-2">
+				</a>
+			</div>
+
+			<div class="item1">
+				<a href="images/img3.jpg" data-lightbox="gallery">
+				  <img src="images/img3.jpg" alt="image-3">
+				</a>
+			</div>
+		</div>
+
+		<div class="row1">
+			<div class="item1">
+				<a href="images/img4.jpg" data-lightbox="gallery">
+				  <img src="images/img4.jpg" alt="image-4">
+				</a>
+			</div>
+
+			<div class="item1">
+				<a  href="images/img5.jpg" data-lightbox="gallery">
+				  <img  src="images/img5.jpg" alt="image-5">
+				</a>
+			</div>
+
+			<div class="item1">
+				<a href="images/img6.jpg" data-lightbox="gallery">
+				  <img src="images/img6.jpg" alt="image-6">
+				</a>
+			</div>
+
+			<div class="item1">
+				<a href="images/img7.jpg" data-lightbox="gallery">
+				  <img src="images/img7.jpg" alt="image-7">
+				</a>
+			</div>
+		</div>
+		<button class="black-button" style="margin-top: 20px">show more</button>
+		</center>
+
+
+
+	</div>
+
+</body>
+</html>
+
+<script>
+	
+
+</script>
